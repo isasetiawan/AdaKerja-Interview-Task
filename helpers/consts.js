4fbf40097ab4b2a1426394ecee87dbf34f2cb4ef
isasetiawan <azaqokay@gmail.com>
2019-07-07 14:28:30 +0700

    Create consts.js
    consts.js : create a const to determine user response is yes or no

---
+++ helpers/consts.js
@@ -0,0 +1,19 @@
+const constanta = {
+    yes_asnwers: [
+        "yes",
+        "yeah",
+        "sure",
+        "yap",
+        "yep",
+        "yup",
+        "ok",
+    ],
+    no_answers :[
+        "no",
+        "nah",
+        "nope",
+        "noh"
+    ]
+}
+
+export default constanta
